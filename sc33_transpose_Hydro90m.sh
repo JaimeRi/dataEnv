98cc608465e32c6b6ca58923a21f1a2a578f863b
Jaime Garcia Marquez <jrgarcia.marquez@gmail.com>
2023-12-16 10:48:53 +0100

    general commit

--- sc33_transpose_Hydro90m.sh
+++ sc33_transpose_Hydro90m.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-export tmp=/mnt/shared/tmp
+export tmp=/mnt/shared/tiles_tb/tmp
 export out=/mnt/shared/tiles_tb
 export zip=/mnt/shared/EnvTablesTiles
 export layers=/mnt/shared/gpfs/gibbs/pi/hydro/hydro/dataproces/ENVTABLES
@@ -23,7 +23,7 @@ tbids=( $(find /mnt/shared/tiles_tb/indx -name "${nm}_*.txt") )
 echo "subcID min max range mean sd" > ${tmp}/${nm}_${var}.txt
 
 # validate table
-echo "${nm}_${var}" > $out/valid/${nm}_${var}.txt
+#echo "${nm}_${var}" > $out/valid/${nm}_${var}.txt
 
 # for loop to go through each RU and extract the ids of interest
 for i in ${tbids[@]}
@@ -31,7 +31,7 @@ do
     # if file is empty go to next one
     [[ ! -s $i ]] && continue
 
-   wc -l < $i >> $out/valid/${nm}_${var}.txt 
+  # wc -l < $i >> $out/valid/${nm}_${var}.txt 
 
     # extract ru number
     ru=$(basename $i .txt | awk -F_ '{print $2}')
@@ -47,11 +47,11 @@ done
 zip -jq $zip/Hydrography90m/${var}/${nm}_${var}.zip \
     ${tmp}/${nm}_${var}.txt
 
-wc -l < ${tmp}/${nm}_${var}.txt >> $out/valid/${nm}_${var}.txt
+#wc -l < ${tmp}/${nm}_${var}.txt >> $out/valid/${nm}_${var}.txt
 
 rm ${tmp}/${nm}_${var}.txt
 
-echo "${nm} ${var} done" >> $out/hydro_tiles_done.txt
+echo "${nm} ${var} done" >> $tmp/hydro_tiles_done.txt
 
 }
 
@@ -60,16 +60,16 @@ echo "${nm} ${var} done" >> $out/hydro_tiles_done.txt
 tile=(h18v02 h20v02 h18v04 h20v04)
 var=(flowpos spi slopgrad)
 
-for t in ${tile[@]}
+for t in $(cat $tmp/tiles.txt)
 do
-    for i in ${var[@]}
+    for i in $(cat $tmp/hydro_vars.txt)
     do
        echo $t $i 
     done 
 done > $tmp/tbtrans_hydro.txt
 
 export -f TransposeTable_Hydro90m
-time parallel -j 12 --colsep ' ' TransposeTable_Hydro90m ::::  $tmp/tbtrans_hydro.txt
+time parallel -j 30 --colsep ' ' TransposeTable_Hydro90m ::::  $tmp/tbtrans_hydro.txt
 
 
 
